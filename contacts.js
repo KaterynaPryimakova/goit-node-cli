20010fda5f437f2348abec33a7c6d877902af06a
KaterynaPryimakova <134732205+KaterynaPryimakova@users.noreply.github.com>
2024-02-20 23:04:51 +0200

    step-2

--- contacts.js
+++ contacts.js
@@ -1,23 +1,65 @@
-import { log } from "console";
 import fs from "fs/promises";
 import path from "path";
+import DetectFileEncodingAndLanguage from "detect-file-encoding-and-language";
+import { nanoid } from "nanoid";
 
 const directory = "D:/Node.js/goit-node-cli/db";
 const filename = "contacts.json";
-export const contactPath = path.resolve(directory, filename);
+const contactPath = path.join(directory, filename);
 
 export async function listContacts() {
-    // ...твій код. Повертає масив контактів.
+    const fileCode = await DetectFileEncodingAndLanguage(contactPath);
+    const contacts = JSON.parse(await fs.readFile(contactPath, fileCode));
+    return contacts;
 }
 
 export async function getContactById(contactId) {
-    // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+    const contacts = await listContacts();
+    const result = contacts.find((contact) => contact.id === contactId);
+    console.log(result);
+    return result || null;
 }
-
-export async function removeContact(contactId) {
-    // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
-}
+// getContactById("qdggE76Jtbfd9eWJHrssH");
 
 export async function addContact(name, email, phone) {
-    // ...твій код. Повертає об'єкт доданого контакту (з id).
+    const contacts = await listContacts();
+    const newContact = {
+        id: nanoid(),
+        name,
+        email,
+        phone,
+    };
+    contacts.push(newContact);
+    await fs.writeFile(contactPath, JSON.stringify(contacts, null, 2));
+    console.log(newContact);
+    return newContact;
+}
+// addContact("Allen Raymond", "nulla.ante@vestibul.co.uk", "(992) 914-3792");
+export async function updateContact(contactId, name, email, phone) {
+    const contacts = await listContacts();
+    const index = contacts.findIndex((contact) => contact.id === contactId);
+    if (index === -1) {
+        return null;
+    }
+    contacts[index] = { contactId, name, email, phone };
+    await fs.writeFile(contactPath, JSON.stringify(contacts, null, 2));
+    console.log(contacts[index]);
+    return contacts[index];
+}
+// updateContact(
+//     "AeHIrLTr6JkxGE6SN-0Rw",
+//     "Allen Raymond",
+//     "nulla.ante@vestibul.co.uk",
+//     "(999) 999-9999"
+// );
+export async function removeContact(contactId) {
+    const contacts = await listContacts();
+    const index = contacts.findIndex((contact) => contact.id === contactId);
+    if (index === -1) {
+        return null;
+    }
+    const [result] = contacts.splice(index, 1);
+    await fs.writeFile(contactPath, JSON.stringify(contacts, null, 2));
+    console.log(result);
+    return result;
 }
--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 import { program } from "commander";
+import {
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact,
+    updateContact,
+} from "./contacts.js";
 program
     .option("-a, --action <type>", "choose action")
     .option("-i, --id <type>", "user id")
@@ -14,19 +21,28 @@ const options = program.opts();
 async function invokeAction({ action, id, name, email, phone }) {
     switch (action) {
         case "list":
-            // ...
+            const contacts = await listContacts();
             break;
 
         case "get":
-            // ... id
+            const oneContact = await getContactById(id);
             break;
 
         case "add":
             // ... name email phone
+            const newContact = await addContact(name, email, phone);
             break;
 
         case "remove":
-            // ... id
+            const removedContact = await removeContact(id);
+            break;
+
+        case "update":
+            const updatedContact = await updateContact(id, {
+                name,
+                email,
+                phone,
+            });
             break;
 
         default:
